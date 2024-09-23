c1ca480092a84a7954b6ae6c4317ce5380c3be41
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-09-23 15:57:42 +0200

    Update prepocesssing.py

--- prepocesssing.py
+++ prepocesssing.py
@@ -4,11 +4,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.preprocessing import LabelBinarizer
 
 class DataProcessor:
     def __init__(self):
         self.train_df=pd.read_csv('sign_mnist_train/sign_mnist_train.csv')
         self.test_df=pd.read_csv('sign_mnist_test/sign_mnist_test.csv')
+        #divisione etichette e dati
+        self.train_label=self.train_df['label']
+        trainset=self.train_df.drop(['label'],axis=1)
+        self.test_label=self.test_df['label']
+        testset=self.test_df.drop(['label'],axis=1)
+        #creazione array numpy
+        self.X_train = trainset.values
+        self.X_test = testset.values
+        
+        #resize
+        self.X_train = self.X_train.reshape(-1,28,28,1)
+        self.X_test = self.X_test.reshape(-1,28,28,1)
+        #convertire etichette in binario
+        lb=LabelBinarizer()
+        self.y_train=lb.fit_transform(self.train_label)
+        self.y_test=lb.fit_transform(self.test_label)
         
 
     def train_info(self):
@@ -31,7 +48,23 @@ class DataProcessor:
         print('Esempio di dati nel dataframe di test')
         print(self.test_df.head())
 
-Processor = DataProcessor()
-Processor.train_info()
-Processor.test_info()
-Processor.train_sample()
+    def view_Xtrain(self):
+        print(self.X_train.shape)
+        print(self.X_train)
+
+    def print_shapes(self):
+        print('X_test:', self.X_test.shape)
+        print('X_train:', self.X_train.shape)
+        print('y_test:', self.y_test.shape)
+        print('y_train:', self.y_train.shape)
+
+    def get_datas(self):
+        return self.X_train, self.X_test, self.y_train, self.y_test
+
+if __name__ == 'main':
+    Processor = DataProcessor()
+    Processor.print_shapes()
+    # Processor.train_info()
+    # Processor.test_info()
+    # Processor.train_sample()
+    # Processor.view_Xtrain()
